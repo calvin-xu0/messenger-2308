6ea966f39c8c1be7c6480e1e96ef847cdf0847cb
Calvin Xu <calvin.xu.jy@gmail.com>
2021-11-26 18:18:41 -0500

    Use async/await keywords over Promise APIs

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -93,18 +93,17 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body)
-      .then( result => {
-        if (!body.conversationId) {
-          dispatch(addConversation(body.recipientId, result.message));
-        } else {
-          dispatch(setNewMessage(result.message));
-        }
+    const data = await saveMessage(body);
 
-        sendMessage(result, body);
-      })
+    if (!body.conversationId) {
+      dispatch(addConversation(body.recipientId, data.message));
+    } else {
+      dispatch(setNewMessage(data.message));
+    }
+
+    sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
