84506bf256e9a0e8749052fe738745518120fa62
Calvin Xu <calvin.xu.jy@gmail.com>
2021-11-26 17:53:19 -0500

    Improve consistency across reducer functions

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,12 +13,10 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      const newMessages = convo.messages;
-      newMessages.push(message);
-      return {...convo,
-        messages: newMessages,
-        latestMessageText: message.text
-      };
+      const convoCopy = { ...convo };
+      convoCopy.messages.push(message);
+      convoCopy.latestMessageText = message.text;
+      return convoCopy;
     } else {
       return convo;
     }
@@ -72,13 +70,11 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      const newMessages = convo.messages;
-      newMessages.push(message);
-      return {...convo,
-        id: message.conversationId,
-        messages: newMessages,
-        latestMessageText: message.text
-        };
+      const convoCopy = { ...convo };
+      convoCopy.id = message.conversationId;
+      convoCopy.messages.push(message);
+      convoCopy.latestMessageText = message.text;
+      return convoCopy;
     } else {
       return convo;
     }
